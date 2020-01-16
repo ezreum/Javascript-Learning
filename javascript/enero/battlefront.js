507e0b3c2aea0ba638c8103e9c91c40018ec02a8
ezreum <whismaster_power@hotmail.com>
2020-01-16 12:40:36 +0100

    checkear

--- javascript/enero/battlefront.js
+++ javascript/enero/battlefront.js
@@ -9,12 +9,16 @@ function cortarLista(miform, boton) {
     for (let i = 0; i < seleccion.options.length; i++) {
         var tamano = noSeleccion.options.length;
         var opt = new Option(seleccion[i].text,seleccion[i].value,"","");
-        seleccion[i].selected==''?'':(noSeleccion[tamano]= opt) && (seleccion[i]=null);
-        var tamano = noSeleccion.options.length;
+        seleccion[i].selected==''?'':(noSeleccion[tamano]= opt) ;
     }
     
-   
-     
+    //bajamos desde el último ya que los indices se reescriben al cambiar opciones
+
+    for (let i = (seleccion.options.length-1); i >= 0 ; i--) {
+        seleccion[i].selected==''?'':seleccion[i]=null;
+    }
+    
+
     /* var tamano = noSeleccion.options.length;
     for (let i = 0; i < seleccionados.length; i++) {
         document.getElementById("midiv").innerHTML+= seleccionados[i].name
@@ -24,12 +28,63 @@ function cortarLista(miform, boton) {
 
 }
 
-function cortarTodo(miform) {
+function cortarTodo(miform, boton) {
     
+    //clasificacion
+    var selector = boton.name;
+    var seleccion=selector=="temp"?miform.temporal:miform.permanente;
+    
+     //buscamos el no seleccionado
+    var noSeleccion = boton.name=="temp"?miform.permanente:miform.temporal;
+    
+    for (let i = 0; i < seleccion.options.length; i++) {
+        var tamano = noSeleccion.options.length;
+        var opt = new Option(seleccion[i].text,seleccion[i].value,"","");
+        noSeleccion[tamano]= opt;
+    }
+    
+    for (let i = (seleccion.options.length-1); i >= 0 ; i--) {
+        seleccion[i]=null;
+    }
+
 }
+
+
+function copiarLista(miform, boton) {
+    //clasificacion
+    var selector = boton.name;
+    var seleccion=selector=="temp"?miform.temporal:miform.permanente;
+    
+     //buscamos el no seleccionado
+    var noSeleccion = boton.name=="temp"?miform.permanente:miform.temporal;
+    
+    for (let i = 0; i < seleccion.options.length; i++) {
+        var tamano = noSeleccion.options.length;
+        var opt = new Option(seleccion[i].text,seleccion[i].value,"","");
+        seleccion[i].selected==''?'':(noSeleccion[tamano]= opt) ;
+    }
+
+}
+
+function borrarLista(miform, boton) {
+    //clasificacion
+    var selector = boton.name;
+    var seleccion=selector=="temp"?miform.temporal:miform.permanente;
+    
+     //buscamos el no seleccionado
+    var noSeleccion = boton.name=="temp"?miform.permanente:miform.temporal;
+    
+    for (let i = (seleccion.options.length-1); i >= 0 ; i--) {
+        var tamano = noSeleccion.options.length;
+        var opt = new Option(seleccion[i].text,seleccion[i].value,"","");
+        seleccion[i].selected==''?'':seleccion[i]= null ;
+    }
+
+}
+
+
 function entrar(miform) {
     var introducir = miform.introducir.value;
-    var select = miform.temporal;
     var tamano = miform.temporal.options.length;
     var opt = new Option(introducir,introducir,"","");
     miform.temporal[tamano] = opt;
