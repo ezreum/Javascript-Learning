444ae5c31faa636b4b659c3ff66c0a370db6b208
ezreum <whismaster_power@hotmail.com>
2020-01-31 12:32:26 +0100

    a

--- javascript/enero/formDOM.js
+++ javascript/enero/formDOM.js
@@ -1,45 +1,57 @@
 
 
-function cabeceraText() {
-    var entrada = document.getElementsByTagName("input");
-    var nombre;
-    if (entrada[0].value != 'undefined' && entrada[0].value!='') {
-        nombre=entrada[0].value;
-        document.getElementsByTagName("select")[0].disabled='';
-    }
-    else{
-        document.getElementsByTagName("select")[0].disabled=true;
-        entrada[1].disabled=true;
-    }
-}
-
 function cabeceraNivel() {
     var info = document.getElementsByTagName("select")[0];
     var res = parseInt(info.value);
     var entrada = document.getElementsByTagName("input");
-    if (res != 'undefined' && entrada[0].value!='') {
-    entrada[1].disabled=false;
+    if (res != 'undefined') {
+    entrada[0].disabled=false;
     }
     else{
-    entrada[1].disabled=true;
+    entrada[0].disabled=true;
     }
 }
 
 function hacerCabeceras() {
     var entrada = document.getElementsByTagName("input");
-    var nombre = entrada[0].value;
     var info = document.getElementsByTagName("select")[0].value;
-    var cantidad = entrada[1].value;
-    if (nombre!='' && info != 'undefined' && cantidad!= 'undefined') {
-        for (var numero of cantidad) {
-           var caja = document.createElement("input");
-           var atrib1 = document.createTextNode("hoooola");
-           caja.appendChild(atrib1);
-           document.getElementsByTagName("body")[0].insertBefore(caja,document.getElementById("caja"));
+    var cantidad = entrada[0].value;
+    if (info != 'undefined' && cantidad!= 'undefined') {
+            var etiqueta = document.createElement("label");
+            var text= document.createTextNode("nombre cabecera");
+            etiqueta.appendChild(text);
+            var caja = document.createElement("input");
+           caja.type="text";
+           caja.onchange=function () {
+            cabecera(this, etiqueta, cantidad);
         }
+           document.getElementsByTagName("body")[0].insertBefore(etiqueta,document.getElementById("cabeceras"));
+           document.getElementsByTagName("body")[0].insertBefore(caja,document.getElementById("cabeceras"));
         
     }
     else{
         document.getElementById("miDiv").innerHTML="algún dato está mal, por favor revísalo";
     }
+}
+var i=0;
+function cabecera(caja, etiqueta, cantidad) {
+    var nombre = caja.value;
+    var info = document.getElementsByTagName("select")[0].value;
+    
+       
+        if (nombre!='undefined' && nombre !='' && i < cantidad) {
+            var cabecera = document.createElement("h"+info+"");
+            var text = document.createTextNode(nombre);
+            cabecera.appendChild(text);
+            document.getElementsByTagName("body")[0].appendChild(cabecera);
+            
+            i++;
+        }  
+        else {
+            etiqueta.parentNode.removeChild(etiqueta);
+            caja.parentNode.removeChild(caja);
+        }
+
+    
+    
 }
