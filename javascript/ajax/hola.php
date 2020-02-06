b885964530eae04c2084214a4feeba870f08d820
ezreum <whismaster_power@hotmail.com>
2020-02-06 12:44:40 +0100

    todo

--- javascript/ajax/hola.php
+++ javascript/ajax/hola.php
@@ -1,1 +1,36 @@
-echo"<h1>hooooola</h1>";
+$paises = [
+St Kitts & Nevis,
+St Lucia,
+Saint Vincent & the Grenadines,
+Samoa,
+San Marino,
+Sao Tome & Principe,
+Saudi Arabia,
+Senegal,
+Serbia,
+Seychelles,
+Sierra Leone,
+Singapore,
+Slovakia,
+Slovenia,
+Solomon Islands,
+Somalia,
+South Africa,
+South Sudan,
+Spain,
+Sri Lanka,
+Sudan,
+Suriname,
+Swaziland,
+Sweden,
+Switzerland,
+Syria
+    ];
+
+    $respuesta='';
+    for($i=0; $i<3; $i++){
+$aleatorio =(rand(1,25))-1;
+    $respuesta+=$paises[$aleatorio];
+    $respuesta+="#";
+    }
+    echo $respuesta;
--- javascript/ajax/js/ejerDivs.js
+++ javascript/ajax/js/ejerDivs.js
@@ -2,12 +2,15 @@
 var peticion="";
 var divs = document.getElementsByTagName("div");
 
+setInterval("lanzarAjax()",3000);
 function lanzarAjax() {
+   
     if (window.XMLHttpRequest) {
         peticion = new XMLHttpRequest();
         respuesta();
     }
     else if(window.ActiveXObject){
+        
         peticion= new ActiveXObject("Microsoft.XMLHTTP");
         var check = respuesta();
         check?alert("su proceso de ajax se proceso correctamente"):alert("No se proceso ajax correctamente");
@@ -16,17 +19,20 @@ function lanzarAjax() {
 
 function respuesta() {
     var bool = false;
+    var aleatoria=Math.random();
     peticion.onreadystatechange=consultar;
-    peticion.open("GET", "../php/paises.php")
+    peticion.open("GET", "../php/paises.php"+"?aleatorio="+aleatoria);
     peticion.send(null);
 }
 
 function consultar() {
     var bol = false;
     if (peticion.readyState==4 && peticion.status==200) {
+        var res= peticion.responseText.split("#");
+        var i=0;
         for (var div of divs) {
-            div.innerHTML=peticion.responseText;
-            console.log("a");
+           div.innerHTML=res[i];
+            i++;
         }
         bol=true;
     }
