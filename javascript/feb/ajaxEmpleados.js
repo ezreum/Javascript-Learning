3dd2a766908bc231e00af627715a5891638bbe58
ezreum <whismaster_power@hotmail.com>
2020-02-19 12:35:21 +0100

    todo

--- javascript/feb/ajaxEmpleados.js
+++ javascript/feb/ajaxEmpleados.js
@@ -29,33 +29,23 @@ function consultar() {
     if (peticion.readyState==4 && peticion.status==200) {
         var res= JSON.parse(peticion.responseText);
 
-        for (i in res) { 
-            var par=document.createElement("p");
-            var texto=document.createTextNode(res[i].display);
-            var texto1=document.createTextNode(res[i].display);
-            par.appendChild(texto);
-            var a=document.createElement("a");
-            a.href=res[i].url;
-            a.target="_blank";
-            a.appendChild(texto1);
-            var espacio= document.createElement("hr");
-            document.getElementsByTagName("body")[0].appendChild(par);
-            document.getElementsByTagName("body")[0].appendChild(a);
-            document.getElementsByTagName("body")[0].appendChild(espacio);
-        }
-
 
     }
     
 }
 
 function checkear() {
+    var check = false;
     var contenido = document.getElementsByTagName("input")[0].value.trim().toUpperCase();
     var patron=/^[0-5][0-9][0-9][B-DF-HJ-NPR-TVX-Z]{3}$/;
     console.log(patron.test(contenido));
     if (patron.test(contenido)) {
         document.getElementById("miDiv").innerHTML=contenido;
+        check=true;
     } else {
         document.getElementById("miDiv").innerHTML="¡Algo fue mal en la validación!";
+    }
+    if (check){
+        lanzarAjax();
     }
 }
