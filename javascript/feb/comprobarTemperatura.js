d61c357fe217552d67ff1334a8b70d60e402e82c
ezreum <whismaster_power@hotmail.com>
2020-02-26 08:28:05 +0100

    todo

--- javascript/feb/comprobarTemperatura.js
+++ javascript/feb/comprobarTemperatura.js
@@ -34,22 +34,82 @@ function respuesta(archivo) {
 function consultar() {
     
     if (peticion.readyState==4 && peticion.status==200) {
-        //var res= document.getElementsByTagName("empleados");
+        document.getElementById("miDiv").innerHTML="";
         var res=peticion.responseXML;
         var raiz=res.getElementsByTagName("prediccion");
         var dias=res.getElementsByTagName("dia");
       
         var provincia=res.getElementsByTagName("provincia")[0].firstChild.nodeValue;
 
-        var html = "<table class='table-active'>"
-        html+="<tr><th>Lugar</th><th>Mínima</th><th>Máxima</th></tr>"
-        for (let i = 0; i < dias.length; i++) {
-            var maxima = dias[i].getElementsByTagName("temperatura")[0].getElementsByTagName("maxima")[0].firstChild.data;
-            var minima = dias[i].getElementsByTagName("temperatura")[0].getElementsByTagName("minima")[0].firstChild.nodeValue;
-            html+="<tr><td>"+provincia+"</td><td>"+maxima+"</td><td>"+minima+"</td></tr>";
+        var tabla = document.createElement("table");
+        tabla.style="border-spacing: 10px; border-collapse: separate;";
+        tabla.class="ml-1";
+        var tr=document.createElement("tr");
+        var th = document.createElement("th");
+        var texto = document.createTextNode("Provincia");
+        th.append(texto);
+        tr.append(th);
+        
+        var tede;
+        var texto2;
+        for (let index = 0; index < dias.length; index++) {
+            tede=document.createElement("td");
+            texto2=document.createTextNode(provincia);
+            tede.appendChild(texto2);
+            tr.appendChild(tede);
         }
-        html+="</table>"
-        document.getElementById("miDiv").innerHTML=html;
+        tabla.append(tr);
+
+        tr=document.createElement("tr");
+        th = document.createElement("th");
+       texto=document.createTextNode("dia");
+       th.append(texto);
+        tr.append(th);
+            for (let i = 0; i < dias.length; i++) {
+                var dia = dias[i].getAttribute("fecha");
+                dia = new Date(dia);
+                tede=document.createElement("td");
+        texto2=document.createTextNode(dia.getDate()+"/"+(dia.getMonth()+1)+"/"+dia.getFullYear());
+        tede.appendChild(texto2);
+        tr.appendChild(tede);
+    }
+    tabla.append(tr);
+
+
+            tr=document.createElement("tr");
+            th = document.createElement("th");
+           texto=document.createTextNode("Máxima");
+           th.append(texto);
+            tr.append(th);
+                for (let i = 0; i < dias.length; i++) {
+                    var maxima = dias[i].getElementsByTagName("temperatura")[0].getElementsByTagName("maxima")[0].firstChild.data;
+                    tede=document.createElement("td");
+            texto2=document.createTextNode(maxima);
+            tede.appendChild(texto2);
+            tr.appendChild(tede);
+        }
+        tabla.append(tr);
+
+               
+               
+                tr=document.createElement("tr");
+                th = document.createElement("th");
+                texto=document.createTextNode("Mínima");
+                th.append(texto);
+                tr.append(th);
+            
+                for (let i = 0; i < dias.length; i++) { 
+                    var minima = dias[i].getElementsByTagName("temperatura")[0].getElementsByTagName("minima")[0].firstChild.nodeValue;
+                    tede=document.createElement("td");
+            texto2=document.createTextNode(minima);
+            tede.appendChild(texto2);
+            tr.appendChild(tede);
+        }
+        tabla.append(tr);
+
+            
+       
+        document.getElementById("miDiv").appendChild(tabla);
     }
     
 }
